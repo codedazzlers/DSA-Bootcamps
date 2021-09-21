e64d58413eb3d899dd794fe90f62521ee8cfdc45
Vipul <vipulka2000@gmail.com>
2021-09-21 23:07:39 +0530

    Assignment No. 0 Done

---
+++ Vipul Kumar/if else/01_check_if_rectangle_or_not.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int l,b;
+cin>>l>>b;
+if (l==b)
+{
+    cout<<"it is rectangle";
+}
+else
+{
+    cout<<"it is not a rectangle";
+}
+
+
+return 0;
+}
