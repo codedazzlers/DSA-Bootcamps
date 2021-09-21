b0a400b65162b79338b4378025734f070341c7c4
Arnav Sharma <arnav1776@gmail.com>
2021-09-21 18:04:51 +0530

    initial commit

---
+++ Arnav Sharma/Assignment-0/if-else/square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class square{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length : ");
+        int l = sc.nextInt();
+        System.out.print("Enter the breadth : ");
+        int b = sc.nextInt();
+        if(l == b)
+            System.out.println("it's a square");
+        else
+            System.out.println("not a square");
+        sc.close();
+    }
+}
