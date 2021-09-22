309552b997c76ee1cb22189e59c5ebd693f4e2a3
unknown <you@example.com>
2021-09-22 22:56:20 +0530

    Loops done

---
+++ Sawtantar/Loops/alphabet.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    char i ;
+cout<<"Alphabets from a to z are"<<endl;
+for(i=97;i<=122;i++)
+{
+    cout<<i<<"\t";
+}
+
+cout<<endl;
+
+cout<<"Alphabets from A to Z are"<<endl;
+for(i=65;i<=90;i++)
+{
+    cout<<i<<"\t";
+}
+
+}
---
+++ Sawtantar/Loops/evennumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int n,count=0;
+ cout<< "Enter a limit of even numbers to count: ";
+ cin>>n;
+ cout<<endl<< "Even numbers are:"<<endl;
+ while(count<=n)
+ {
+    cout<< count <<"\t";
+    count+=2;
+ }
+}
