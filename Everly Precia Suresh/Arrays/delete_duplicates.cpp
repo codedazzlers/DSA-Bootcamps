1eb51da385684c7f634ae11cba1c927fc8a6ca2a
Everly Precia Suresh <77877486+everly-gif@users.noreply.github.com>
2021-09-26 11:06:21 +0530

    Update delete_duplicates.cpp

--- Everly Precia Suresh/Arrays/delete_duplicates.cpp
+++ Everly Precia Suresh/Arrays/delete_duplicates.cpp
@@ -4,12 +4,11 @@ int main(){
     int n;
     cout<<"Enter number of elements : ";
     cin>>n;
-    int ar[n],freq[n];
+    int ar[n];
     cout<<"Enter Elements: ";
 
     for(int i=0;i<n;i++){ //read
         cin>>ar[i];
-        freq[i]=-1;
     }
     cout<<"Array inputted successfully"<<endl;
     int count;
