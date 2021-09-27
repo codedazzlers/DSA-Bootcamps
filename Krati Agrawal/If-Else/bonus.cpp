7d215c641a5d6913c11548475fa72b2bc61984fd
Krati Agrawal <68179652+kraticodes@users.noreply.github.com>
2021-09-27 12:58:03 +0530

    Update bonus.cpp
    Changed salary input type to float.

--- Krati Agrawal/If-Else/bonus.cpp
+++ Krati Agrawal/If-Else/bonus.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-    int salary, years;
+    float salary, years;
     cout << "Enter Salary : ";
     cin >> salary;
     cout << "Enter Years of Service : ";
     cin >> years;
     if (years > 5)
     {
-        int bonus = (salary * 5) / 100;
+        float bonus = (salary * 5) / 100;
         cout << "The net bonus amount is" << bonus << endl;
     }
     else
