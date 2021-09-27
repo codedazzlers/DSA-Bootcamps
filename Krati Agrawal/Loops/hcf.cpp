dfcc038dcf85301778a451a916a77e51d8ba4f9f
Krati Agrawal <68179652+kraticodes@users.noreply.github.com>
2021-09-27 13:10:15 +0530

    Update hcf.cpp
    Corrected the code.

--- Krati Agrawal/Loops/hcf.cpp
+++ Krati Agrawal/Loops/hcf.cpp
@@ -8,7 +8,9 @@ int main(){
     cin>>num2;
     min=(num1<num2)? num1: num2;
     for(int i=1;i<=min;i++){
-        hcf=i;
+        if (num1 % i == 0 && num2 % i ==0){
+            hcf=i;
+        }
     }
     cout<<"HCF of given number is= "<<hcf<<endl;
     return 0;
