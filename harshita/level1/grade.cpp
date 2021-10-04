c00b19478de9c0123047652b332f4c509621efa8
harshita214 <79797000+harshita214@users.noreply.github.com>
2021-10-04 16:41:01 +0530

    Update grade.cpp

--- harshita/level1/grade.cpp
+++ harshita/level1/grade.cpp
@@ -9,27 +9,26 @@ int main()
     cout << "Enter your marks: ";
     cin>> marks ;
     if(marks > 80){
-        grade=A;
         cout << "  yey you got A!";
     }
     else if(marks>=60 && marks<=80 ){
-        grade=B;
+        
         cout << "  yey you got B!";
     }
     else if(marks>=50 && marks<=60){
-        grade=C;
+       
         cout <<  "  yey you got C!";
     }
     else if(marks>45 && marks<50){
-        grade=D;
+        
         cout <<  "   you got D!";
     }
     else if(marks>25&&marks<45){
-        grade=E;
+       
         cout << "   you got E!";
     }
     else if(marks<25){
-        grade=F;
+       
         cout << "you got F!";
     }
     else{
