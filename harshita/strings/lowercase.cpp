f6f1b4ebc2ccd3016ae40eb1dc97bac333326834
harshita214 <79797000+harshita214@users.noreply.github.com>
2021-10-06 14:22:09 +0530

    from global folder transferred files to my folder (#54)
    * Create Q11,Q12.md
    
    * Rename BackwardString.cpp to harshita/strings/BackwardString.cpp
    
    * Rename Compare1.cpp to harshita/strings/compare.cpp
    
    * Rename ConcatenatedString.cpp to harshita/strings/ConcatenatedString.cpp
    
    * Rename LengthOfString.cpp to harshita/strings/LengthOfString.cpp
    
    * Rename NoOfWords.cpp to harshita/strings/NoOfWords.cpp
    
    * Delete 11,12answer.txt
    
    * Rename ReverseString.cpp to harshita/strings/ReverseString.cpp
    
    * Rename lowercase.cpp to harshita/strings/lowercase.cpp
    
    * Rename palindrome.cpp to harshita/strings/palindrome.cpp
    
    * Rename uppercase.cpp to harshita/strings/uppercase.cpp

--- harshita/strings/lowercase.cpp
+++ harshita/strings/lowercase.cpp
@@ -5,10 +5,10 @@ int main(){
     string str;
     cout<<"Enter a string : ";
     getline(cin,str);
-    cout<<"String in Upper case: ";
+    cout<<"String in Lower case: ";
     for(int i=0;str[i]!=0;i++){
-        if(str[i]>='a' && str[i] <='z'){
-            str[i]=str[i]-32;
+        if(str[i]>='A' && str[i] <='Z'){
+            str[i]=str[i]+32;
             cout<<str[i];
         }
         else{
